0cf0e6cf02719506e7269b0f6bbc56992cfdd0b7
Lucas Walter <wsacul@gmail.com>
2025-12-29 16:19:09 -0800

    make test_no_std work with uom clothoid

--- test_no_std/src/main.rs
+++ test_no_std/src/main.rs
@@ -1,14 +1,16 @@
 pub use clothoid_bezier::f64::ClothoidBezierApproximation;
 
+use uom::si::{f32::Length, length::meter};
+
 fn main() {
     let mut cd = test_no_std::Clothoid::default();
 
-    cd.x0 = 1.0 as test_no_std::Float;
+    cd.xy0.x = Length::new::<meter>(1.0 as test_no_std::Float);
 
-    let mut cd = ClothoidBezierApproximation {
+    let cd = ClothoidBezierApproximation {
         curvature: 1.0,
         curvature_rate: 0.1,
         length: 10.0,
     };
-    let clothoid = cd.to_clothoid();
+    let _clothoid = cd.to_clothoid();
 }
